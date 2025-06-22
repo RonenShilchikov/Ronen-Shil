8c85b0314c184c5623a49093c5413f1cfd8a79d8
Ronen Shil <rshilchikov@gmail.com>
2025-06-22 11:01:59 +0300

    add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,10 @@ export default function Document() {
     <Html lang="en">
       <Head>
 
+          <link rel="icon" href="/favicon.ico" />
+         {/* Optional for modern browsers */}
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+
         {/* Google Font: JetBrains Mono */}
         <link
           href="https://fonts.googleapis.com/css2?family=JetBrains+Mono&family=Roboto&display=swap"
